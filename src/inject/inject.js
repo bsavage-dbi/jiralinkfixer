8ccbc38200b999d3bda58d65fb6ae13da9ca0892
Chris Whittle <chris@richcontext.com>
2019-08-01 09:54:27 -0500

    changed to poll for url changes due to when on certain screens and you open a modal those links were not being fixed

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,23 +1,34 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		
-    // ----------------------------------------------------------
-    // All external links should open in new tab/window. Internal links are relative.
-    var anchors = document.querySelectorAll('a');
-    var linksFixed = 0;
-    for (var i = 0; i < anchors.length; i++) {
-      if (anchors[i].host !== window.location.hostname) {
-        linksFixed = linksFixed + 1;
-        anchors[i].setAttribute('target', '_blank');
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      
+      // ----------------------------------------------------------
+      // All external links should open in new tab/window. Internal links are relative.
+      let location = window.location.href;;
+      function fixURLS() {
+          var anchors = document.querySelectorAll('a');
+          var linksFixed = 0;
+          for (var i = 0; i < anchors.length; i++) {
+            if (anchors[i].host !== window.location.hostname) {
+              linksFixed = linksFixed + 1;
+              anchors[i].setAttribute('target', '_blank');
+            }
+          }
+          console.log("JIRA Link Fixer - " + linksFixed + " link" + (linksFixed > 1 ? "s" : "") + " fixed!");
       }
-    }
 
-    console.log("JIRA Link Fixer - Loaded, " + linksFixed + " link" + (linksFixed > 1 ? "s" : "") + " fixed!");
-	}
+      setInterval(() => {
+        if(location !== window.location.href) {
+          location = window.location.href;
+          setTimeout(fixURLS, 1000);
+        }
+      }, 100);
+      console.log("JIRA Link Fixer - Loaded");
+      fixURLS();
+    }
 	}, 10);
 });
